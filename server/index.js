af6b600ed3ae7a9a974f466d9e078494929c09e7
nehalRayuu <99276671+nehalRayuu@users.noreply.github.com>
2023-05-19 21:47:24 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 
 import Connection from './database/db.js';
 import Routes from './routes/route.js';
+dotenv.config();
 
 const app = express();
 
@@ -15,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/', Routes);
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 Connection();
 
