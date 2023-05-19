ca9645a1b6c3802f813390ccf18b65c183c0cc52
nehalRayuu <99276671+nehalRayuu@users.noreply.github.com>
2023-05-19 22:39:54 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -7,8 +7,8 @@ dotenv.config();
 
 const Connection = () => {
     mongoose.set('strictQuery', true);
-    const MONGODB_URI = process.env.URL
-    mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+    const MONGODB_URL = process.env.MONGODB_URL
+    mongoose.connect(MONGODB_URL, { useNewUrlParser: true });
 
     mongoose.connection.on('connected', () => {
         console.log('Database connected Successfully');
